71bcccf790df5d5ddfad4d52e94edf5b90749baf
Samba <midhunnagasaicse@gmail.com>
2024-10-16 11:24:37 +0530

    Messages Component COMPLETED

--- src/pages/sub-components/Messages.jsx
+++ src/pages/sub-components/Messages.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import {
+  clearAllMessageErrors,
+  deleteMessage,
+  getAllMessages,
+  resetMessageSlice,
+} from "../../store/slices/messagesSlice";
+import { toast } from "react-toastify";
 
 const Messages = () => {
   const navigateTo = useNavigate();
-  const handleReturnToDashboard = () => {
-    navigateTo("/");
-  };
+  // const handleReturnToDashboard = () => {
+  //   navigateTo("/");
+  // };
 
   const { loading, messages, error, message } = useSelector(
     (state) => state.messages
@@ -14,36 +21,88 @@ const Messages = () => {
 
   const [messageId, setMessageId] = useState("");
 
+  const dispatch = useDispatch();
+  const handleMessageDelete = (id) => {
+    setMessageId(id);
+    dispatch(deleteMessage(id));
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch(clearAllMessageErrors());
+    }
+    if (message) {
+      toast.success(message);
+      dispatch(resetMessageSlice());
+      dispatch(getAllMessages());
+    }
+  }, [dispatch, error, message, loading]);
+
   return (
     <>
       <div className="m-3 p-3">
         <div class="card">
           <div class="card-header">
             <span className="fs-4">Messages</span>
-            <button
+            {/* <button
               onClick={handleReturnToDashboard}
               className="float-end btn btn-outline-dark"
             >
               Return to Dashboard
-            </button>
+            </button> */}
           </div>
-          <div class="card-body">
+          <div class="card-body row">
             {messages && messages.length > 0 ? (
               messages.map((ele) => {
                 return (
-                  <div key={ele._id} className="row">
-                    <h5 class="col-md-6 card-title">Sender Name:</h5>
-                    <p class="col-md-6 card-text">
-                     {ele.senderName}
-                    </p>
-                    <h5 class="col-md-6 card-title">Subject:</h5>
-                    <p class="col-md-6 card-text">
-                     {ele.subject}
-                    </p>
-                    <h5 class="col-md-6 card-title">Message:</h5>
-                    <p class="col-md-6 card-text">
-                     {ele.message }
-                    </p>
+                  <div key={ele._id} className="col-md-4">
+                    <div className="card m-3" style={{ width: "18rem;" }}>
+                      <ul class="list-group list-group-flush">
+                        <li className="list-group-item">
+                          <div className="row">
+                            <span className="col-md-5 fw-bold">
+                              Sender Name:
+                            </span>
+                            <span className="col-md-7"> {ele.senderName}</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item">
+                          <div className="row">
+                            <span className="col-md-5 fw-bold">Subject:</span>
+                            <span className="col-md-7"> {ele.subject} </span>
+                          </div>
+                        </li>
+                        <li class="list-group-item">
+                          <div className="row">
+                            <span className="col-md-5 fw-bold">Message:</span>
+                            <span className="col-md-7"> {ele.message} </span>
+                          </div>
+                        </li>
+                        <li class="list-group-item">
+                          {loading && messageId === ele._id ? (
+                            <div>
+                              <span>Deleting</span>
+                              <div
+                                className="spinner-border text-danger"
+                                role="status"
+                              >
+                                <span className="visually-hidden">
+                                  Loading...
+                                </span>
+                              </div>
+                            </div>
+                          ) : (
+                            <button
+                              className="btn btn-outline-danger float-end"
+                              onClick={() => handleMessageDelete(ele._id)}
+                            >
+                              Delete
+                            </button>
+                          )}
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 );
               })
--- src/store/slices/messagesSlice.js
+++ src/store/slices/messagesSlice.js
@@ -25,6 +25,27 @@ const messageSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    deleteMessageRequest(state, action) {
+      state.message = null;
+      state.error = null;
+      state.loading = true;
+    },
+    deleteMessageSuccess(state, action) {
+      state.message = action.payload;
+      state.error = null;
+      state.loading = false;
+    },
+    deleteMessageFailed(state, action) {
+      state.message = null;
+      state.error = action.payload;
+      state.loading = false;
+    },
+    resetMessageSlice(state, action) {
+      state.error = null;
+      state.messages = state.messages;
+      state.message = null;
+      state.loading = false;
+    },
     clearAllErrors(state, action) {
       state.error = null;
       state.messages = state.messages;
@@ -39,11 +60,36 @@ export const getAllMessages = () => async (dispatch) => {
       "http://localhost:4000/api/v1/message/getall",
       { withCredentials: true }
     );
-    dispatch(messageSlice.actions.getAllMessagesSuccess(data.messages))
-    dispatch(messageSlice.actions.clearAllErrors())
+    dispatch(messageSlice.actions.getAllMessagesSuccess(data.messages));
+    dispatch(messageSlice.actions.clearAllErrors());
   } catch (error) {
-    dispatch(messageSlice.actions.getAllMessagesFailed(error.response.data.message))
+    dispatch(
+      messageSlice.actions.getAllMessagesFailed(error.response.data.message)
+    );
   }
 };
 
-export default messageSlice.reducer
+export const deleteMessage = (id) => async (dispatch) => {
+  dispatch(messageSlice.actions.deleteMessageRequest());
+  try {
+    const { data } = await axios.delete(
+      `http://localhost:4000/api/v1/message/delete/${id}`,
+      { withCredentials: true }
+    );
+    dispatch(messageSlice.actions.deleteMessageSuccess(data.message))
+    dispatch(messageSlice.actions.clearAllErrors())
+    
+  } catch (error) {
+    dispatch(messageSlice.actions.deleteMessageFailed(error.response.data.message))
+  }
+};
+
+export const clearAllMessageErrors = () => async(dispatch) => {
+  dispatch(messageSlice.actions.clearAllErrors())
+}
+
+export const resetMessageSlice = () => (dispatch) => {
+  dispatch(messageSlice.actions.resetMessageSlice())
+}
+
+export default messageSlice.reducer;
